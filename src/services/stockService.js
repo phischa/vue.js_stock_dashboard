95f4d0370e9cf51a8f708a345c2f9589b9ed369f
phischa <ph.git@mailbox.org>
2025-11-28 12:16:17 +0100

    added array with stock names

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,5 +1,25 @@
 import axios from 'axios'
 
+/* const order = [
+    'Mar 21',
+    'Jun 21',
+    'Sep 21',
+    'Dec 21',
+    'Mar 22',
+    'Jun 22',
+    'Sep 22',
+    'Dec 22',
+    'Mar 23',
+    '""',
+    '3 Aug 23',
+    '2 Nov 23',
+    '1 Feb 24',
+    '2 Mai 24'
+];
+ */
+
+const STOCKS = ['AAPL', 'AMZN', 'GOOG', 'META', 'MSFT', 'NVDA', 'TSLA']
+
 const apiClient = axios.create({
     baseURL: 'https://sheetdb.io/api/v1/hm3fanqlxlavs',
     timeout: 10000,
@@ -12,6 +32,13 @@ export async function getStockData(sheet) {
     const response = await apiClient.get('', { params: { sheet: `$${sheet}` } })
     return response.data
 }
+
+/* export async function getOrderedStockData(sheet) {
+    const data = await getStockData(sheet)
+    data.sort((a, b) => {
+        return order.indexOf(a.Date) - order.indexOf(b.Date)
+    })
+} */
 
 export async function getAllStocks() {
     const response = await apiClient.get('')
